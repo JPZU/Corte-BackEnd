2d5ff463ebc5891a8b5b3526efc892ccb7e101b4
JPZU <juanpablozp@icloud.com>
2025-02-26 11:41:35 -0500

    first commit

---
+++ src/main/java/com/arapos/corte/CorteApplication.java
@@ -0,0 +1,13 @@
+package com.arapos.corte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arapos/corte/CorteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arapos.corte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
