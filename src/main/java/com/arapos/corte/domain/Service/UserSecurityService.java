94c11ab20b5e1aae43d2b21937d0c5c80c25b86e
JPZU <juanpablozp@icloud.com>
2025-04-28 17:34:36 -0500

    Setting of all values for each http method

--- src/main/java/com/arapos/corte/domain/Service/UserSecurityService.java
+++ src/main/java/com/arapos/corte/domain/Service/UserSecurityService.java
@@ -3,11 +3,16 @@ package com.arapos.corte.domain.Service;
 import com.arapos.corte.domain.repository.UserRepository;
 import com.arapos.corte.persistence.crud.UserCrudRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import javax.management.relation.Role;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserSecurityService implements UserDetailsService {
--- src/main/java/com/arapos/corte/web/config/SecurityConfig.java
+++ src/main/java/com/arapos/corte/web/config/SecurityConfig.java
@@ -18,9 +18,72 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.GET, "/users/*").hasAnyRole("ADMIN", "SUPER_ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/users/*").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "/users/*").hasAnyRole("ADMIN", "SUPER_ADMIN")
+                        /* --------------------------------------------------------
+                                            CATEGORIES CONTROLLER
+                        --------------------------------------------------------- */
+                        .requestMatchers(HttpMethod.GET, "/categories/**").hasAnyRole("SUPER_ADMIN", "ADMIN", "EDITOR", "VIEWER")
+                        .requestMatchers(HttpMethod.POST, "/categories/*").hasAnyRole("SUPER_ADMIN", "ADMIN", "EDITOR")
+                        .requestMatchers(HttpMethod.PUT , "/categories/*").hasAnyRole("SUPER_ADMIN", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE , "/categories/*").hasAnyRole("SUPER_ADMIN")
+
+                        /* --------------------------------------------------------
+                                            CLOTHS CONTROLLER
+                        --------------------------------------------------------- */
+                        .requestMatchers(HttpMethod.GET, "/cloths/**").hasAnyRole("SUPER_ADMIN", "ADMIN", "EDITOR", "VIEWER")
+                        .requestMatchers(HttpMethod.POST, "/cloths/*").hasAnyRole("SUPER_ADMIN", "ADMIN", "EDITOR")
+                        .requestMatchers(HttpMethod.PUT , "/cloths/*").hasAnyRole("SUPER_ADMIN", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE , "/cloths/*").hasAnyRole("SUPER_ADMIN")
+
+                        /* --------------------------------------------------------
+                                            FULL OP CONTROLLER
+                        --------------------------------------------------------- */
+                        .requestMatchers(HttpMethod.POST, "/full-op/*").hasAnyRole("SUPER_ADMIN", "ADMIN", "EDITOR")
+
+                        /* --------------------------------------------------------
+                                            ITEM CLOTHS CONTROLLER
+                        --------------------------------------------------------- */
+                        .requestMatchers(HttpMethod.GET, "/item-cloths/**").hasAnyRole("SUPER_ADMIN", "ADMIN", "EDITOR", "VIEWER")
+                        .requestMatchers(HttpMethod.PUT , "/item-cloths/*").hasAnyRole("SUPER_ADMIN", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE , "/item-cloths/*").hasAnyRole("SUPER_ADMIN")
+
+                        /* --------------------------------------------------------
+                                            ITEM REFERENCES CONTROLLER
+                        --------------------------------------------------------- */
+                        .requestMatchers(HttpMethod.GET, "/item-references/**").hasAnyRole("SUPER_ADMIN", "ADMIN", "EDITOR", "VIEWER")
+                        .requestMatchers(HttpMethod.PUT , "/item-references/*").hasAnyRole("SUPER_ADMIN", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE , "/item-references/*").hasAnyRole("SUPER_ADMIN")
+
+                        /* --------------------------------------------------------
+                                            OPS CONTROLLER
+                        --------------------------------------------------------- */
+                        .requestMatchers(HttpMethod.GET, "/ops/**").hasAnyRole("SUPER_ADMIN", "ADMIN", "EDITOR", "VIEWER")
+                        .requestMatchers(HttpMethod.PUT , "/ops/*").hasAnyRole("SUPER_ADMIN", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE , "/ops/*").hasAnyRole("SUPER_ADMIN")
+
+                        /* --------------------------------------------------------
+                                            REFERENCES CONTROLLER
+                        --------------------------------------------------------- */
+                        .requestMatchers(HttpMethod.GET, "/references/**").hasAnyRole("SUPER_ADMIN", "ADMIN", "EDITOR", "VIEWER")
+                        .requestMatchers(HttpMethod.POST, "/references/*").hasAnyRole("SUPER_ADMIN", "ADMIN", "EDITOR")
+                        .requestMatchers(HttpMethod.PUT , "/references/*").hasAnyRole("SUPER_ADMIN", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE , "/references/*").hasAnyRole("SUPER_ADMIN")
+
+                        /* --------------------------------------------------------
+                                            SUPPLIERS CONTROLLER
+                        --------------------------------------------------------- */
+                        .requestMatchers(HttpMethod.GET, "/suppliers/**").hasAnyRole("SUPER_ADMIN", "ADMIN", "EDITOR", "VIEWER")
+                        .requestMatchers(HttpMethod.POST, "/suppliers/*").hasAnyRole("SUPER_ADMIN", "ADMIN", "EDITOR")
+                        .requestMatchers(HttpMethod.PUT , "/suppliers/*").hasAnyRole("SUPER_ADMIN", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE , "/suppliers/*").hasAnyRole("SUPER_ADMIN")
+
+                        /* --------------------------------------------------------
+                                            USERS CONTROLLER
+                        --------------------------------------------------------- */
+                        .requestMatchers(HttpMethod.GET, "/users/**").hasAnyRole("SUPER_ADMIN", "ADMIN", "VIEWER")
+                        .requestMatchers(HttpMethod.POST, "/users/*").hasAnyRole("SUPER_ADMIN", "ADMIN")
+                        .requestMatchers(HttpMethod.PUT , "/users/*").hasAnyRole("SUPER_ADMIN", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE , "/users/*").hasAnyRole("SUPER_ADMIN")
+
                         .anyRequest()
                         .authenticated()
                 )
