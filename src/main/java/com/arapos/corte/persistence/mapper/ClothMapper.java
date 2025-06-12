c0ef6c40f1aa63384bb98d4c83bec1996c37dbd1
JPZU <juanpablozp@icloud.com>
2025-06-12 18:46:21 -0500

    updated mapper of cloth

--- src/main/java/com/arapos/corte/persistence/mapper/ClothMapper.java
+++ src/main/java/com/arapos/corte/persistence/mapper/ClothMapper.java
@@ -4,11 +4,9 @@ import com.arapos.corte.domain.dto.Cloth.ClothResponseDTO;
 import com.arapos.corte.domain.dto.Cloth.CreateClothDTO;
 import com.arapos.corte.persistence.entity.Cloth;
 import com.arapos.corte.persistence.entity.Category;
-import com.arapos.corte.persistence.entity.Supplier;
-import com.arapos.corte.persistence.entity.User;
 import org.mapstruct.*;
 
-@Mapper(componentModel = "spring", uses = {UserMapper.class, CategoryMapper.class, SupplierMapper.class})
+@Mapper(componentModel = "spring", uses = {CategoryMapper.class})
 public interface ClothMapper {
     /* --------------------------------------------------------
                     ENTITY -> RESPONSEDTO
@@ -19,20 +17,14 @@ public interface ClothMapper {
     --------------------------------------------------------- */
             @Mapping(source = "clothId", target = "clothId"),
             @Mapping(source = "name", target = "name"),
-            @Mapping(source = "color", target = "color"),
             @Mapping(source = "meters", target = "meters"),
+            @Mapping(source = "isActive", target = "isActive"),
             @Mapping(source = "createdAt", target = "createdAt"),
             @Mapping(source = "updatedAt", target = "updatedAt"),
-            @Mapping(source = "isActive", target = "isActive"),
-            @Mapping(source = "notes", target = "notes"),
-            @Mapping(source = "price", target = "price"),
-            @Mapping(source = "supplierInvoice", target = "supplierInvoice"),
     /* --------------------------------------------------------
                     relationships
     --------------------------------------------------------- */
-            @Mapping(source = "user", target = "user"), // Usa UserMapper
             @Mapping(source = "category", target = "category"), // Usa CategoryMapper
-            @Mapping(source = "supplier", target = "supplier"), // Usa SupplierMapper
     /* --------------------------------------------------------
                         unmapped
     --------------------------------------------------------- */
@@ -48,19 +40,14 @@ public interface ClothMapper {
     --------------------------------------------------------- */
             @Mapping(source = "clothId", target = "clothId"),
             @Mapping(source = "name", target = "name"),
-            @Mapping(source = "color", target = "color"),
             @Mapping(source = "meters", target = "meters"),
             @Mapping(source = "isActive", target = "isActive"),
-            @Mapping(source = "notes", target = "notes"),
-            @Mapping(source = "price", target = "price"),
-            @Mapping(source = "supplierInvoice", target = "supplierInvoice"),
     /* --------------------------------------------------------
                     relationships entity
     --------------------------------------------------------- */
             @Mapping(target = "itemClothsList", ignore = true), // Evita mapear relaciones OneToMany
-            @Mapping(target = "user", expression = "java(mapUser(createClothDTO.getUserId()))"), // Convierte ID a entidad
+            @Mapping(target = "clothEntryItemsList", ignore = true), // Evita mapear relaciones OneToMany
             @Mapping(target = "category", expression = "java(mapCategory(createClothDTO.getCategoryId()))"), // Convierte ID a entidad
-            @Mapping(target = "supplier", expression = "java(mapSupplier(createClothDTO.getSupplierId()))"),// Convierte ID a entidad
     /* --------------------------------------------------------
                         unmapped
     --------------------------------------------------------- */
@@ -72,11 +59,6 @@ public interface ClothMapper {
     /* --------------------------------------------------------
                 AUXILIARY METHODS TO MAPPER ENTITIES
     --------------------------------------------------------- */
-    default User mapUser(int userId) {
-        User user = new User();
-        user.setUserId(userId);
-        return user;
-    }
 
     default Category mapCategory(int categoryId) {
         Category category = new Category();
@@ -84,9 +66,4 @@ public interface ClothMapper {
         return category;
     }
 
-    default Supplier mapSupplier(String supplierId) {
-        Supplier supplier = new Supplier();
-        supplier.setSupplierId(supplierId);
-        return supplier;
-    }
 }
